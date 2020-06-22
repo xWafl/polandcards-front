77b205958af9846833516b8bc184488464da1ea0
xWafl <73sampleperson@gmail.com>
2020-06-22 12:00:59 -0400

    :wrench: Game route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import Game from "@/views/Game.vue";
 
 Vue.use(VueRouter);
 
@@ -18,7 +19,12 @@ const routes: Array<RouteConfig> = [
         // which is lazy-loaded when the route is visited.
         component: () =>
             import(/* webpackChunkName: "about" */ "../views/About.vue")
-    }
+    },
+    {
+        path: "/game",
+        name: "Game",
+        component: Game
+    },
 ];
 
 const router = new VueRouter({
