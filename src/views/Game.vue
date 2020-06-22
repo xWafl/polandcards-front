f36ec81a0cb99fe3d334111ddad594bc7cf64c90
xWafl <73sampleperson@gmail.com>
2020-06-22 09:12:16 -0400

    :art: Formatting

---
+++ src/views/Game.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="game"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Game extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
